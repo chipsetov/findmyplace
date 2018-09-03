e6fd9603da33107362e2d711fd3e01f34453a814
yura_bart <y.bartoshchuk@gmail.com>
2018-09-03 19:41:59 +0300

    Make a basic project skeleton

---
+++ src/main/java/ua/softserve/rv036/findmeplace/FindMePlaceApplication.java
@@ -0,0 +1,12 @@
+package ua.softserve.rv036.findmeplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMePlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMePlaceApplication.class, args);
+	}
+}
