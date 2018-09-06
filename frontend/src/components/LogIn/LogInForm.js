3b030056909efaca3e92b58cf5b5f151476dffac
Oleksandra Kaminska <oleksandra.kaminska28@gmail.com>
2018-09-06 23:42:49 +0200

    Add LogIn component

---
+++ frontend/src/components/LogIn/LogInForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Input, Button, Row } from 'react-materialize';
+import { Link } from 'react-router-dom';
+
+class LogInForm extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    render() {
+        return (
+            <div className="form">
+                <Row>
+                    <Input
+                        id="email"
+                        type="email"
+                        label="Email"
+                        value={this.state.email}
+                        //onChange={}
+                        s={12}
+                    />
+                </Row>
+                <Row>
+                    <Input
+                        id="password"
+                        type="password"
+                        label="Password"
+                        value={this.state.password}
+                        //onChange={}
+                        s={12}
+                    />
+                     <Link className="forgot-password-link" to="/">Forgot password?</Link>
+                </Row>
+                <Button waves="light" /*onClick={}*/>Sign In</Button>
+            </div>
+        );
+    }
+
+}
+
+export default LogInForm;
