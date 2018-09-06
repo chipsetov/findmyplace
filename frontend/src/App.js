df2e4a24f3da70b8b7f2f160ea667f85c144277f
Oleksandra Kaminska <oleksandra.kaminska28@gmail.com>
2018-09-06 23:45:23 +0200

    Modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import LogInForm from './components/LogIn/LogInForm.js';
+//import RegistrationForm from './components/Registration/RegistrationForm.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <LogInForm/>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
