3d641eb535a0119760a4e69052e79a94566159b7
yura_bart <y.bartoshchuk@gmail.com>
2018-09-06 10:13:01 +0300

    Change docker compsoe file to rebuild spring-boot

--- api/src/main/java/ua/softserve/rv036/findmeplace/Run.java
+++ api/src/main/java/ua/softserve/rv036/findmeplace/Run.java
@@ -2,9 +2,17 @@ package ua.softserve.rv036.findmeplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController
 public class Run {
+
+   /* @RequestMapping("/")
+    public String init(){
+        return "HELLO JAVA WORLD!";
+    }*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Run.class, args);
