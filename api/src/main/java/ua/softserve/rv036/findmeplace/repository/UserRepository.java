bd9781fcc1dadd7f63f29278bc3ec1f058d502e1
yura_bart <y.bartoshchuk@gmail.com>
2018-09-07 12:14:57 +0300

    Implement UserRepository

--- api/src/main/java/ua/softserve/rv036/findmeplace/repository/UserRepository.java
+++ api/src/main/java/ua/softserve/rv036/findmeplace/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package ua.softserve.rv036.findmeplace.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.softserve.rv036.findmeplace.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
