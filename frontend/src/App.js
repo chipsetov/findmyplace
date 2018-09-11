604a0aa40a17a5cbecfe47207eddd84e10d38002
yura_bart <y.bartoshchuk@gmail.com>
2018-09-11 11:55:47 +0300

    front-map-bad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import LoginForm from './components/Login/LoginForm.js';
+//import LoginForm from './components/Login/LoginForm.js';
 //import RegistrationForm from './components/Registration/RegistrationForm.js';
 //import AuthenticationBar from './components/Header/AuthenticationBar.js';
-
+import MapForm from './components/Map/MapForm.js';
 
 class App extends Component {
 
     render() {
         return (
             <BrowserRouter>
-                <div className="app">
-                    <LoginForm/>
+                <div id="mapid" className="app">
+                    <MapForm/>
                 </div>
             </BrowserRouter>
         );
