cc6961763ade8b64a316667b2bfe99b1c4d8c121
Oleksandra Kaminska <sasha249133@ukr.net>
2018-09-12 23:00:54 +0200

    Changed menu styles

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 import { Navbar, NavItem, Button } from 'react-materialize';
-import { Link } from 'react-router-dom';
 import '../styles/Menu.css';
 
 class Menu extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="menu-bar">
-                    <div className="logo-wrapper">
-                        <Link className="logo" to="/">
-                            <img src="./img/logo.png" alt="Find Me Place"/>
-                        </Link>
-                    </div>
-                    <div className="menu-wrapper">
-                        <Navbar className="menu" right>
-                            <NavItem href=''>Home</NavItem>
-                            <NavItem href=''>Map</NavItem>
-                            <NavItem href=''>About us</NavItem>
-                            <NavItem href='' id="auth-sign-up">
-                                <Button waves="light">
-                                    Sign Up
-                                </Button>
-                            </NavItem>
-                            <NavItem href='' id="auth-sign-in">Sign In</NavItem>
-                        </Navbar>
-                    </div>
+            <div className="menu-container">
+                <div className="menu-wrapper">
+                    <Navbar brand="logo" className="menu" right>
+                        <NavItem href=''>Home</NavItem>
+                        <NavItem href=''>Map</NavItem>
+                        <NavItem href=''>About us</NavItem>
+                        <NavItem href='' id="auth-sign-up">
+                            <Button waves="light">
+                                Sign Up
+                            </Button>
+                        </NavItem>
+                        <NavItem href='' id="auth-sign-in">Sign In</NavItem>
+                    </Navbar>
                 </div>
             </div>
         );
