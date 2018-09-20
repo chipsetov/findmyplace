46fd406b6a2ff0e98f14bd2720761a1ae7838087
sergiy-oliynik <sergiy.oliynik.93@gmail.com>
2018-09-20 12:14:54 +0300

    Added user's page

---
+++ frontend/src/components/UserPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class UserPage extends Component {
+    render() {
+        return (
+            <h1>User's page</h1>
+        );
+    }
+}
