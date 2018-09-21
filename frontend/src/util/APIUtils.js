87c26ed802269a59b8708808c28798707ac316af
yura_bart <y.bartoshchuk@gmail.com>
2018-09-21 13:55:13 +0300

    Add post method for filter place

--- frontend/src/util/APIUtils.js
+++ frontend/src/util/APIUtils.js
@@ -39,6 +39,14 @@ export function signup(signupRequest) {
     });
 }
 
+export function filterPlace(filterRequest) {
+    return request({
+        url: API_BASE_URL + "/map",
+        method: 'POST',
+        body: JSON.stringify(filterRequest)
+    });
+}
+
 export function checkUsernameAvailability(username) {
     return request({
         url: API_BASE_URL + "/user/checkUsernameAvailability?username=" + username,
