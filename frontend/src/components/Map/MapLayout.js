0262789b43e9c93d93e6246ab85d9ad347d4ea61
yura_bart <y.bartoshchuk@gmail.com>
2018-09-27 12:20:40 +0300

    add Place card to Popup of markers

--- frontend/src/components/Map/MapLayout.js
+++ frontend/src/components/Map/MapLayout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Map, Marker, Popup, TileLayer, Tooltip} from 'react-leaflet'
-import {Link} from 'react-router-dom';
+import Place from "../../components/User/Place/Place";
 import '../../styles/Map.css';
 
 
@@ -26,10 +26,14 @@ export default class MapLayout extends Component {
                             <Tooltip direction="top">
                                 {place.name}
                             </Tooltip>
-                            <Popup>
-                                <span> {place.name} </span>
-                                <span> Free place: {place.countFreePlaces} </span>
-                                <span> <Link to={`/place/${place.id}`}>Place page</Link> </span>
+                            <Popup className="places-container">
+                                <Place id={place.id}
+                                       name={place.name}
+                                       placeType={place.placeType.name}
+                                       description={place.description}
+                                       rating={place.rating}
+                                       countFreePlaces={place.countFreePlaces}
+                                />
                             </Popup>
                         </Marker>
                     ))}
