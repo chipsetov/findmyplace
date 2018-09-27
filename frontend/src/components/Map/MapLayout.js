469cd2879139f3e2f08abfc1cac1a30e44bd8ab2
yura_bart <y.bartoshchuk@gmail.com>
2018-09-27 11:40:01 +0300

    add ToolTip to Markers

--- frontend/src/components/Map/MapLayout.js
+++ frontend/src/components/Map/MapLayout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
+import {Map, Marker, Popup, TileLayer, Tooltip} from 'react-leaflet'
 import {Link} from 'react-router-dom';
 import '../../styles/Map.css';
 
@@ -17,12 +17,15 @@ export default class MapLayout extends Component {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     <Marker position={currentPosition}>
-                        <Popup>
+                        <Tooltip direction="top" permanent={true}>
                             This is your current location.
-                        </Popup>
+                        </Tooltip>
                     </Marker>
                     {this.props.items.map(place => (
                         <Marker key={place.name} position={[place.latitude, place.longitude]}>
+                            <Tooltip direction="top">
+                                {place.name}
+                            </Tooltip>
                             <Popup>
                                 <span> {place.name} </span>
                                 <span> Free place: {place.countFreePlaces} </span>
