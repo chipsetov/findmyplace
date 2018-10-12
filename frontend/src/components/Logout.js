8dff1cc06ce02b7a1eca7835412165908e71cb6a
Oleksandra Kaminska <sasha249133@ukr.net>
2018-10-12 20:47:34 +0200

    Add admin and base dropdown

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -16,7 +16,32 @@ export default class Logout extends Component {
         this.props.logout();
     }
 
-    viewPlaces () {
+    adminDropdown() {
+        return (
+            <Dropdown trigger={<img src="img/avatar.png" alt=""/>}>
+                <NavItem href='#/admin-page'>Dashboard</NavItem>
+                <NavItem href='#/logout' onClick={this.onClickHandler}>
+                    Sign out
+                </NavItem>
+            </Dropdown>
+        )
+    };
+
+    baseDropdown() {
+        return (
+            <Dropdown trigger={<img src="img/avatar.png" alt=""/>}>
+                <NavItem href='#/user/profile'>Profile</NavItem>
+                {this.viewPlaces()}
+                <NavItem href='#'>Booking</NavItem>
+                <NavItem href='#'>Favorite</NavItem>
+                <NavItem href='#/logout' onClick={this.onClickHandler}>
+                    Sign out
+                </NavItem>
+            </Dropdown>
+        )
+    };
+
+    viewPlaces = () => {
         if(Session.isOwner()) {
             const userId = Session.userId();
             return (
@@ -31,15 +56,7 @@ export default class Logout extends Component {
         return (
             <div id="auth-sign-out" className={hidden}>
                 <NavItem href="" className="logout">
-                    <Dropdown trigger={<img src="img/avatar.png" alt=""/>}>
-                        <NavItem href='#/user/profile'>Profile</NavItem>
-                            {this.viewPlaces()}
-                        <NavItem href='#'>Booking</NavItem>
-                        <NavItem href='#'>Favorite</NavItem>
-                        <NavItem href='#/logout' onClick={this.onClickHandler}>
-                            Sign out
-                        </NavItem>
-                    </Dropdown>
+                    {Session.isAdmin() ? this.adminDropdown() : this.baseDropdown()}
                 </NavItem>
             </div>
         );
