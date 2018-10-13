7ad20f4d2be6909c862eb159f19029c928f4528e
yura_bart <y.bartoshchuk@gmail.com>
2018-10-13 12:43:40 +0300

    add simple test

--- api/src/test/java/ua/softserve/rv036/findmeplace/controller/UserControllerTest.java
+++ api/src/test/java/ua/softserve/rv036/findmeplace/controller/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ua.softserve.rv036.findmeplace.controller.UserController;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -26,6 +27,12 @@ public class UserControllerTest {
     @Test
     public void deleteUserPlaceById() throws Exception {
         mvc.perform(delete("/user/delete-place/{id}", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getManagerPlacesTest() throws Exception {
+        mvc.perform(get("/manager/{id}/places", 1L))
                 .andExpect(status().isOk());
     }
 
