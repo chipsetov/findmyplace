7d4e9d7b6f083d31795ab7568ce4dabbafa72fec
sergiy-oliynik <sergiy.oliynik.93@gmail.com>
2018-10-15 23:51:40 +0300

    Added method sendBookingConfirmation

--- api/src/main/java/ua/softserve/rv036/findmeplace/service/UserServiceImpl.java
+++ api/src/main/java/ua/softserve/rv036/findmeplace/service/UserServiceImpl.java
@@ -65,4 +65,12 @@ public class UserServiceImpl implements UserService {
 
         }
     }
+
+    public void sendBookingConfirmation(User user) throws IOException {
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = "Your booking was approved!";
+
+            mailSender.send(user.getEmail(), "Your booking was approved!", message);
+        }
+    }
 }
