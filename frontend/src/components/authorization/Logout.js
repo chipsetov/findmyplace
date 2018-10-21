6ea77436e987ca8485e67f680beb727d584b6c3c
sergiy-oliynik <sergiy.oliynik.93@gmail.com>
2018-10-21 23:07:29 +0300

    Small fixes

--- frontend/src/components/authorization/Logout.js
+++ frontend/src/components/authorization/Logout.js
@@ -55,7 +55,7 @@ class Logout extends Component {
     managerView() {
         return (
             <Dropdown trigger={<img src={this.props.userAvatar} alt=""/>}>
-                <NavItem href='#/manager-page'>Places</NavItem>
+                <NavItem href='#/manager-page'>Dashboard</NavItem>
                 <NavItem href='#/logout' onClick={this.props.handleLogout}>
                     Sign out
                 </NavItem>
