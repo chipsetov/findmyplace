3d0d7f68d79d86c768a962c14813ad46fb046387
DeKapito <kapitula_ak16@nuwm.edu.ua>
2018-11-07 22:11:11 +0200

    Design BanPage

--- frontend/src/components/BanPage.js
+++ frontend/src/components/BanPage.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import '../styles/BanPage.css'
+import {Link} from "react-router-dom";
 
 class BanPage extends Component {
     render() {
         return(
-            <div className="ban-page">
-                <div className="container">
-                    <p>You banned</p>
+            <div className="ban-wrapper">
+                <div className="ban-container">
+                    <div className="title">
+                        <h1>You are banned</h1>
+                    </div>
+                    <Link to="/contact-us">
+                        <h2>write to administrator</h2>
+                    </Link>
                 </div>
             </div>
         );
