ee619cc049052e7e639e3712615ef949580b8a06
DeKapito <kapitula_ak16@nuwm.edu.ua>
2018-11-13 12:19:49 +0200

    Added unit tests

--- api/src/test/java/ua/softserve/rv036/findmeplace/controller/UserControllerTest.java
+++ api/src/test/java/ua/softserve/rv036/findmeplace/controller/UserControllerTest.java
@@ -1,7 +1,9 @@
 package ua.softserve.rv036.findmeplace.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,13 +11,27 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ua.softserve.rv036.findmeplace.controller.UserController;
+import ua.softserve.rv036.findmeplace.model.Place;
+import ua.softserve.rv036.findmeplace.model.PlaceVisitHistory;
 import ua.softserve.rv036.findmeplace.payload.EmailToAdminRequest;
+import ua.softserve.rv036.findmeplace.security.UserPrincipal;
 import ua.softserve.rv036.findmeplace.utils.TestUtil;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -28,6 +44,16 @@ public class UserControllerTest {
 
     @MockBean
     private UserController userController;
+
+    private PlaceVisitHistory visitHistoryItem;
+
+    private List<PlaceVisitHistory> visitHistoryItems;
+
+    @Before
+    public void setup(){
+        visitHistoryItem = new PlaceVisitHistory(1l, 1l, 1l, new Date());
+        visitHistoryItems = Collections.singletonList(visitHistoryItem);
+    }
 
     @Test
     public void deleteUserById() throws Exception {
@@ -76,6 +102,21 @@ public class UserControllerTest {
         mvc.perform(post("/email/to-user")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(query))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteVisitHistory() throws Exception {
+        mvc.perform(delete("/user/visit-history/delete/1")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void userHistoryVisit() throws Exception {
+
+        given(userController.userHistoryVisit(Mockito.mock(UserPrincipal.class))).willReturn(visitHistoryItems);
+
+        mvc.perform(get("/user/visit-history")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
     }
 }
